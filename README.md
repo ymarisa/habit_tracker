88881f82dc85880148f771b1165e7066860995b1
Marisa Yeung <neurosa@gmail.com>
2019-08-16 13:53:56 -0700

    documentation, cleaning up

--- README.md
+++ README.md
@@ -10,28 +10,31 @@ See your daily progress on habits.
 
 Takes numerical and boolean values.
 
-Mark off days that don't count against your goal.
+Mark off days that don't count against your goal, they will show as striped.
 
 Click a cell or row/header label to view a summary. Click again to toggle off.
 
-Future: Monthly and weekly views.
+Future: Monthly and weekly views, hover for numerical values.
 
-## Grid and flex box used?
+## Grid and flex box
 The main sections title, labels, content and habit summary are defined using grid.
 
 Flexbox makes up the rows of habit data, day labels and day summary results, columns of habit labels and habit summary results.
+
+See `img/grid_overview.jpg` for overview.
  
 ## Transition and Animation
-Future: overlay semi-transparent layer over row/col when hovering over a cell or label.
-Future:  
+Hover adds an opaque layer over the cell.
 
 ## Javascript
-Onclick events for each cell, each row header, and each column header.
+Onclick events for each cell, each row header, and each column header. Clicking a cell will show the row and/or col summaries. Clicking a second time turns summaries off.
 
-Future: clicks will show summaries of results for day and habit.
+Keeping track of when to toggle a summary on or off got tricky so I wrote out the truth tables. See `img/toggle_cell_truth_tables.jpg`. The highlighted cells are the conditions I wrote if statements for. 
+
+Future: clicks will show summaries of results for day and habit instead of TEMP
 
 ## Directory structure
-javascript is the html file for now.
+
 ```
 .
 ├── README.md
@@ -40,8 +43,17 @@ javascript is the html file for now.
 │   └── site.css
 ├── img
 │   ├── grid_overview.jpg
-│   └── habit_tracker_wireframe_-_Google_Slides.jpg
+│   ├── habit_tracker_wireframe_-_Google_Slides.jpg
+│   └── toggle_cell_truth_tables.jpg
 ├── index.html
 └── js
+    └── main.js
 
 ```
+
+## Future
+* Monthly view
+* Allow transition to other weeks
+* Calculate and show row/col summaries
+* Figure out a better way of spacing vertical labels other than line-height
+
--- js/main.js
+++ js/main.js
@@ -12,6 +12,13 @@ function showSummary(day, habitN) {
 
     let dayToggle = false;
     let habitToggle = false;
+
+    /*
+        Turn off summary if the same thing is clicked twice.
+        If cell already shows both summaries, turn them off.
+        If clicking a label and there no summary on the other axis, it's the 2nd click so turn off.
+        See truth tables in img/toggle_cell_truth_tables.jpg
+     */
     if (day !== null && habitN !== null) {
         if (habitSummaryNodes[habitN].innerText !== "" && daySummaryNodes[day].innerText !== "") {
             console.log("clicked same cell, turn off summaries")
@@ -33,94 +40,6 @@ function showSummary(day, habitN) {
         if (!newCellSameRow && daySummaryNodes[day].innerText !== "") {
             console.log("clicked day label with no habit summaries on, turn off habit summary")
             dayToggle = true;
-        }
-    }
-
-    // clear all results
-    habitSummaryNodes.forEach((item) => item.innerText = "");
-    daySummaryNodes.forEach((item) => item.innerText = "");
-
-    // show results
-    if (habitN !== null && !habitToggle) {
-        habitSummaryNodes[habitN].innerText = "TEMP";
-    }
-    if (day !== null && !dayToggle) {
-        daySummaryNodes[day].innerText = "TEMP";
-    }
-}
-
-
-function showSummary2(day, habitN) {
-    console.log("showSummary fn");
-
-
-
-    // get all the flexbox items for each summary
-    let habitSummaryNodes = document.querySelectorAll('.habit-summary');
-    let daySummaryNodes = document.querySelectorAll('.day-summary');
-
-
-    /* onClick
-       - turn everything off
-       - turn on the summary of the row/col clicked
-       - if user clicked the same thing a 2nd time, don't turn on (must check before turning everything off)
-
-       Toggle: turn off if it's already on
-            (1) If user clicked the same cell or label, it should turn summary(ies) off (ie toggle = true)
-            (2) if user clicked same row but new cell, row summary should not turn off (toggle = false)
-            (3) if user clicked same col but new cell, col summary should not turn off (toggle = false)
-    */
-
-    // (1)
-    // Habit summary is on, turn it off
-    let habitToggle = false;
-    if (habitN != null) {
-        console.log(1)
-        if (habitSummaryNodes[habitN].innerText !== "") {
-            habitToggle = true;
-        }
-    }
-
-    // (1)
-    //
-    let dayToggle = false;
-    if (day != null) {
-        console.log(2);
-        if (daySummaryNodes[day].innerText !== "") {
-            dayToggle = true;
-        }
-    }
-
-    // if same different cell but same row or col is clicked, don't toggle the one that's already on
-    if (day != null && habitN != null) {
-        if (habitSummaryNodes[habitN].innerText !== "" && daySummaryNodes[day].innerText === "") {
-            // (2) different day, same habit
-            console.log(2);
-            habitToggle = false;
-        } else if (daySummaryNodes[day].innerText !== "" && habitSummaryNodes[habitN].innerText === "") {
-            // (3) different habit, same day
-            console.log(3);
-            dayToggle = false;
-        }
-    }
-
-    // habit label clicked with habit summary already on
-    if (day === null) {
-        // node list to array, then check if one of the day summaries are on
-        let newCellSameRow = Array.from(daySummaryNodes).some((item) => item.innerText !== "");
-        if (newCellSameRow) {
-            // this will be true bc habit summary is on and so should toggle, but not in this case so don't toggle
-            habitToggle = false;
-        }
-    }
-
-    // day label clicked with day summary already on
-    if (habitN === null) { // day label clicked
-        // node list to array then check if one of the habit summaries are on
-        let newCellSameRow = Array.from(habitSummaryNodes).some((item) => item.innerText !== "");
-        if (newCellSameRow) {
-            // this will be true bc day summary is on and so should toggle, but not in this case so don't toggle
-            dayToggle = false;
         }
     }
 
